707923cd4614204e33fe0e356d89e8427c05dd3c
Yazan Fattal <yazanfattal19@gmail.com>
2026-07-02 02:54:14 +0200

    update index

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const images = document.querySelectorAll(".page img");
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+if (darkModeToggle) {
+  darkModeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    darkModeToggle.textContent =
+      document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+  });
+}
 
 const lightbox = document.createElement("div");
 lightbox.className = "lightbox";
 document.body.appendChild(lightbox);
 
-images.forEach(img => {
+document.querySelectorAll(".page img").forEach((img) => {
   img.addEventListener("click", () => {
-    lightbox.innerHTML = `<img src="${img.src}" alt="">`;
+    lightbox.innerHTML = `<img src="${img.src}" alt="${img.alt}">`;
     lightbox.classList.add("active");
   });
 });
